7cff35face3e3393c41ada10dab9c2961029ae16
Sebastian Krieger <sebastian@nublia.com>
2017-05-03 15:17:45 -0300

    Static HTML documentation on gh-pages repository.

--- pycwt/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-
-from . wavelet import *
-
-__all__ = ['cwt', 'icwt', 'significance', 'xwt', 'wct', 'wct_significance',
-           'mothers', 'Morlet', 'Paul', 'DOG', 'MexicanHat']
-__name__ = 'PyCWT'
-__version__ = '0.3.0a4'
