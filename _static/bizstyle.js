3b9a7024a944af1e2707ce01aed966f4edf19bce
Sebastian Krieger <sebastian@nublia.com>
2017-05-17 15:39:01 -0300

    Updates for pycwt 0.3.0a5
    	modified:   .buildinfo
    	modified:   _static/bizstyle.js
    	modified:   genindex.html
    	modified:   getting_started.html
    	modified:   index.html
    	modified:   py-modindex.html
    	modified:   pycwt.html
    	modified:   search.html
    	modified:   searchindex.js
    	modified:   tutorial.html

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("PyCWT 0.3.0a4 documentation");
+        $("li.nav-item-0 a").text("PyCWT 0.3.0a5 documentation");
     }
 });
