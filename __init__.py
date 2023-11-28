f4df578d2a4247f3cdd957f97172e9b7bd6932b0
Christopher J. Steele <steelec@gmail.com>
2023-11-28 10:07:06 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from .pycwt.wavelet import *
+
+__all__ = ['cwt', 'icwt', 'significance', 'xwt', 'wct', 'wct_significance',
+           'mothers', 'Morlet', 'Paul', 'DOG', 'MexicanHat']
+__name__ = 'PyCWT'
+__version__ = '0.3.0a22'
